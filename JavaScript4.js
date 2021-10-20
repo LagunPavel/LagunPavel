f4fcdf668538276feb2cb29bed37b770c3fa30b0
LagunPavel <91014113+LagunPavel@users.noreply.github.com>
2021-10-20 20:17:52 +0300

    Create JavaScript4.js

---
+++ JavaScript4.js
@@ -0,0 +1,15 @@
+function st(){
+var k=parseInt(document.getElementById("k").value);
+var s=parseInt(document.getElementById("s").value);
+var r=document.getElementById("res");
+if(k<1||s<1){
+	alert("Данные введены не коректно")
+	return false;}
+else
+r.innerHTML=k*s;
+}
+window.document.addEventListener("DOMContentLoaded", function (st) {
+    console.log("DOM fully loaded and parsed");
+    var b = document.getElementById("knopka");
+    b.addEventListener("click", st);
+});
